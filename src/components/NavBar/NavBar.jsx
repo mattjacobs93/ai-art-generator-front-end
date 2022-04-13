232261256e563dce3552abcf9f90d8da268e919a
EugeneR <eugenerillo@gmail.com>
2022-04-13 15:11:26 -0400

    add navbar style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/createimage">Create Image</Link></li>
-            <li><Link to="/blogs">Blogs</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+        <nav className={styles.nav}>
+          <ul className={styles.container}>
+            <div className={styles.leftnav}>
+              <h2><Link to='/blogs'>AI Art Generator</Link></h2>
+            </div>
+            <div className={styles.rightnav}>
+              <li><Link to="/createimage">Create Ai Art</Link></li>
+              <li><Link to="/blogs">Blogs</Link></li>
+              <li><Link to="/profile">My Art</Link></li>
+              <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            </div>
+            
           </ul>
         </nav>
       :
