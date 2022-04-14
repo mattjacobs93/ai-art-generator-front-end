c7195e2f97a6dd1447d8a52db519a421b3966d6b
Anthony Madia <aamadia24@gmail.com>
2022-04-14 11:57:12 -0400

    Image preview - state issues

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -20,7 +20,7 @@ const LoginForm = props => {
     try {
       await authService.login(formData)
       props.handleSignupOrLogin()
-      navigate('/')
+      navigate('/blogs')
     } catch (err) {
       props.updateMessage(err.message)
     }
--- src/pages/CreateImage/CreateImage.jsx
+++ src/pages/CreateImage/CreateImage.jsx
@@ -1,20 +1,28 @@
 import { useState, useEffect } from "react";
 import * as artworkService from '../../services/artworkService.js'
 import { useNavigate } from "react-router-dom";
-import styles from './createImage.module.css'
+import styles from './CreateImage.module.css'
 
 
 const CreateImage = () => {
     const [formData, setFormData] = useState({})
     const navigate = useNavigate()
 
+    const [imgFile, uploadImg] = useState("")
+    const [imgFile2, uploadImg2] = useState("")
+
     const handleChange = (e) => {
-       // console.log(e.target.files[0])
        setFormData({...formData, [e.target.id] : e.target.files[0]})
+
+       console.log(e.target.files)
+       let img1 = URL.createObjectURL(e.target.files[0])
+       console.log(img1)
+       let img2 = URL.createObjectURL(e.target.files[1])
+       uploadImg({...img1}['blob'])
+       uploadImg2({...img2}['blob'])
     }
 
     useEffect(()=>{
-        console.log(formData)
     },[formData])
 
     const handleSubmit = async (e) => {
@@ -45,14 +53,44 @@ const CreateImage = () => {
             <div className={styles.body}>
                 <form>
                     <div className={styles.imagesContainer}>
-                       <div className={styles.contentImage}>
+                       <div>
                            <h3>
                                Content Image: 
                            </h3>
-                        <input id="content-image" type="file" onChange={handleChange} /> <br />
+                        <input 
+                            id="content-image" 
+                            type="file" 
+                            onChange={handleChange} 
+                            accept="image/png"
+                        /> <br />
+                           <div className={styles.contentImage}>
+                        <div className={styles.contentPreview}>
+                            <img 
+                                src={imgFile}
+                                alt="Content Preview" 
+                                className={styles.contentPreviewImage}   
+                            />
+                           </div>
+                        </div>
                     </div>
+                    <div>
+                         <h3>
+                             Style Image:
+                        </h3> 
+                         <input 
+                            id="style-image" 
+                            type="file" 
+                            onChange={handleChange} 
+                        /> <br />
                     <div className={styles.styleImage}>
-                         <h3>Style Image:</h3> <input id="style-image" type="file" onChange={handleChange} /> <br />
+                        <div className={styles.stylePreview}>
+                            <img 
+                                src={imgFile2}
+                                alt="Style Preview" 
+                                className={styles.stylePreviewImage}   
+                            />
+                        </div>
+                    </div>
                     </div> 
                     </div>
                     
